cd524d75f3ae828867a102ae8278dec9214b5d78
AislingHeanue <aislingheanue@gmail.com>
2025-06-03 16:42:39 +0100

    Initial Commit
    `flutter create -e particle_game`

---
+++ README.md
@@ -0,0 +1,3 @@
+# particle_game
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/particle_game/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.particle_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
